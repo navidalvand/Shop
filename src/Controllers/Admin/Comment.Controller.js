2e4d83c61821b825032fb13348bbbcf3ae904bf5
navid <navidalvand@gmail.com>
2023-06-29 16:20:14 +0330

    Refactor: Comment Controller Admin Panel Refactored

--- src/Controllers/Admin/Comment.Controller.js
+++ src/Controllers/Admin/Comment.Controller.js
@@ -1,83 +1,60 @@
+const { ModelHandler } = require("../../Utils/Model-Handler");
 const { Controller } = require("../Controller");
 
-class CommentAdminController extends Controller  {
-    
+class CommentAdminController extends Controller {
+  async createComment(req, res, next) {
+    try {
+      const productID = req.params.id;
+      const userID = req.user._id;
+      const { text } = req.body;
+      if (!text.trim()) throw { status: 400, message: "text cannot be empty" };
 
-    async createComment(req, res, next) {
-        try {
-          const productID = req.params.id;
-          const userID = req.user._id;
-          const { text } = req.body;
-          if (!text.trim()) throw { status: 400, message: "text cannot be empty" };
-    
-          const findProduct = await ModelHandler.getByID(ProductModel, productID);
-          if (!findProduct) throw { status: 404, message: "product not found" };
-          const createComment = await ModelHandler.create(CommentModel, {
-            author: userID,
-            text: text.trim(),
-            product: productID,
-          });
-    
-          const response = new ResponseHandler(res);
-          response.success({
-            data: createComment,
-          });
-        } catch (err) {
-          next(err);
-        }
-      }
-    
+      const findProduct = await ModelHandler.getByID(ProductModel, productID);
+      if (!findProduct) throw { status: 404, message: "product not found" };
+      const createComment = await ModelHandler.create(CommentModel, {
+        author: userID,
+        text: text.trim(),
+        product: productID,
+      });
 
-      async deleteComment(req, res, next) {
-        try {
-          const commentID = req.params.id;
-    
-          const deleteComment = await ModelHandler.delete(CommentModel, {
-            _id: commentID,
-          });
-    
-          if (deleteComment.deletedCount === 0)
-            throw { status: 404, message: "comment not found" };
-    
-          const response = new ResponseHandler(res);
-          response.success({ data: deleteComment });
-        } catch (err) {
-          next(err);
-        }
-      }
-    
+      this.success({
+        data: createComment,
+      });
+    } catch (err) {
+      next(err);
+    }
+  }
 
-      async getCommentsList(req, res, next) {
-        try {
-          const query = req.query;
-          const findComments = await ModelHandler.get(CommentModel, query);
-          if (findComments.length === 0)
-            throw { status: 404, message: "comment not found" };
-          const response = new ResponseHandler(res);
-          response.success({ data: findComments });
-        } catch (err) {
-          next(err);
-        }
-      }
-    
+  async deleteComment(req, res, next) {
+    try {
+      const commentID = req.params.id;
 
+      const deleteComment = await ModelHandler.delete(CommentModel, {
+        _id: commentID,
+      });
 
+      if (deleteComment.deletedCount === 0)
+        throw { status: 404, message: "comment not found" };
 
+      this.success({ data: deleteComment });
+    } catch (err) {
+      next(err);
+    }
+  }
 
-
-
-
-
-
-
-
-
-
-
-
+  async getCommentsList(req, res, next) {
+    try {
+      const query = req.query;
+      const findComments = await ModelHandler.get(CommentModel, query);
+      if (findComments.length === 0)
+        throw { status: 404, message: "comment not found" };
+      this.success({ data: findComments });
+    } catch (err) {
+      next(err);
+    }
+  }
 }
-
 
 module.exports = {
-    CommentAdminController : new CommentAdminController()
-}
+  CommentAdminController: new CommentAdminController(),
+};
