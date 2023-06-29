600a6348d39d046ad4ef54e1a7eb1c170fb26bf6
navid <navidalvand@gmail.com>
2023-06-29 16:01:02 +0330

    Refactor: Auth Controller Response System Refactored

--- src/Controllers/Auth/Auth-controller.js
+++ src/Controllers/Auth/Auth-controller.js
@@ -3,13 +3,11 @@ const { ModelHandler } = require("../../Utils/Model-Handler");
 const { validationResult } = require("express-validator");
 const { hashPass, comparePass } = require("../../Utils/hashPass");
 const { generateToken } = require("../../Utils/token");
-const { ResponseHandler } = require("../../Utils/Response-Handler");
 const { Controller } = require("../Controller");
 
 class AuthController extends Controller {
   async singUp(req, res, next) {
     try {
-      const response = new ResponseHandler(res);
       const result = validationResult(req);
 
       //?                           Validating RequestBody Data [ username, phoneNumber, email, password ]
@@ -46,9 +44,9 @@ class AuthController extends Controller {
       const token = generateToken({ username, role: "USER" });
 
       //?                           Set Token In Cookies (ExpiresIn 24H later)
-      response.setCookie("token", token, { maxAge: 86400000, httpOnly: true });
+      this.setCookie("token", token, { maxAge: 86400000, httpOnly: true });
 
-      response.created({
+      this.created({
         data: user,
       });
     } catch (err) {
@@ -58,7 +56,6 @@ class AuthController extends Controller {
 
   async logIn(req, res, next) {
     try {
-      const response = new ResponseHandler(res);
       const result = validationResult(req);
 
       //?                             Validating Request Body Data [ username, password ]
@@ -66,33 +63,29 @@ class AuthController extends Controller {
         throw { status: 400, message: result.errors };
       const { username, password } = req.body;
 
-
       //?                             Check If User Exist In DB
       const user = await ModelHandler.getOne(UserModel, { username });
       if (!user)
         throw { status: 404, message: "username or password is wrong" };
 
-
-        //?                            Check Password In DB
+      //?                            Check Password In DB
       const compare = comparePass(password, user.password);
       if (!compare)
         throw { status: 404, message: "username or password is wrong" };
 
-        //?                            Generate And Set New Token In Cookie
+      //?                            Generate And Set New Token In Cookie
       const token = generateToken({ username, role: user.role });
-      response.setCookie("token", token, { maxAge: 86400000, httpOnly: true });
-      response.success({ status: 200, message: "you loged in" });
+      this.setCookie("token", token, { maxAge: 86400000, httpOnly: true });
+      this.success({ status: 200, message: "you loged in" });
     } catch (err) {
       next(err);
     }
   }
 
   async logOut(req, res, next) {
-    const response = new ResponseHandler(res);
-
     //?                             Clear The Cookie And Send Response
-    response.clearCookie("token");
-    response.success({ message: "you loged out" });
+    this.clearCookie("token");
+    this.success({ message: "you loged out" });
   }
 }
 
